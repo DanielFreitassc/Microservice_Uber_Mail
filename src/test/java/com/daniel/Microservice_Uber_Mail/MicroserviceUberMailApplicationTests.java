72ee7cab25546281629bba37fc29514f84da7dc8
Daniel Freitas <danielfreitassc@gmail.com>
2023-11-30 19:48:43 -0300

    init

---
+++ src/test/java/com/daniel/Microservice_Uber_Mail/MicroserviceUberMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.Microservice_Uber_Mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUberMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
